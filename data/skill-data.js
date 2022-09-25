8ff4229bcbbe8fc201d16eb0a8501b3023785aa8
Marcus <hymanmarcus95@gmail.com>
2022-09-25 17:42:02 -0400

    enable controller functionality

--- data/skill-data.js
+++ data/skill-data.js
@@ -1,6 +1,6 @@
 const skills = [
-  { text:"Read", done: true, _id: 123456}
-  { text:"Write", done: true, _id: 321654}
+  { text:"Read", done: true, _id: 123456},
+  { text:"Write", done: true, _id: 321654},
   { text:"Sleep", done: false, _id: 789456}
 ]
 
