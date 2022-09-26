a4f92621db7c36dbbeb40bbd5f87a08d45ed68e8
Marcus <hymanmarcus95@gmail.com>
2022-09-25 23:12:38 -0400

    contructedSchema

---
+++ models/skill.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+
+// optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema
+
+const skillSchema = new Schema({
+  text: String,
+  done: Boollean
+})
